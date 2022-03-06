e679610001fc8620327941ef9b880b9c0b52e79b
DaCuteRaccoon <72467156+DaCuteRaccoon@users.noreply.github.com>
2022-03-06 12:38:57 -0800

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,4 @@
+./libre-office/AppRun &
+./stacer/AppRun &
+chromium-browser --no-sandbox --disable-setuid-sandbox --start-maximized https://www.davidfahim.com/get-started-workstation.html
+#npm install puppeteer --save && npm start
